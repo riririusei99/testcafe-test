cff719b58ee26cea1c8ea10d9d47c5ae5bef490e
Ryusei Namai <riry1313@gmail.com>
2020-01-22 23:48:11 +0900

    write PageObject patern

--- tests/simple_spec.js
+++ tests/simple_spec.js
@@ -1,12 +1,12 @@
-import { Selector } from 'testcafe';
+import page from './simple_page.js';
 
 fixture `My First Test`
     .page `http://riririusei99.hatenadiary.jp/`;
 
 test('Search riririusei99', async t => {
     await t
-      .typeText('input[type=text]', 'riririusei99')
-      .click('input[type=submit]');
+      .typeText(page.searchInput, 'riririusei99')
+      .click(page.searchSubmit);
 
     const title = await t.eval(() => document.title);
     await t.expect(title).eql('riririusei99 の検索結果 - riririusei99’s blog');
